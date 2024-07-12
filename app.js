340b282fe7e74ba827e65f0cdedd13e4a6dd2d6d
Rodrigo Sanchez <rodrigosanchez@Rodrigos-MacBook-Air.local>
2024-07-12 18:32:57 -0300

    Installing npm and learning about debugging

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ console.log(routes.someText);
 const server = http.createServer(routes.handler);
 
 server.listen(3000);
+
+// npm init creates package.json into your project.
+// adding start property into package.json to start execution with npm start at terminal.
+
+// installing 3rd party dependency to promptly update my changes without restarting the server. npm install nodemon --save-dev
